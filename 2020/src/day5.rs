cf58091260990ed3603a680bb3216800e1fcdf65
Laguana <laguana@redacted>
2022-01-16 21:39:32 -0800

    Day 5 part 1

---
+++ 2020/src/day5.rs
@@ -0,0 +1,41 @@
+use std::str::FromStr;
+
+#[test]
+fn part1_sample_works() {
+    let input = parse_input("BFFFBBFRRR");
+    assert_eq!(input.passes[0], 567)
+}
+
+struct Input {
+    passes: Vec<u16>,
+}
+
+
+impl FromStr for Input {
+    type Err = std::num::ParseIntError;
+    fn from_str(s: &str) -> std::result::Result<Self, <Self as std::str::FromStr>::Err> {
+        let v: Result<Vec<u16>, std::num::ParseIntError> = s.split("\n")
+            .filter(|l| !l.is_empty())
+            .map(|l| {
+                let n = l.trim()
+                    .replace("B","1").replace("F","0")
+                    .replace("R","1").replace("L","0");
+                    u16::from_str_radix(&n[..], 2)
+            }).collect();
+            v.map(|v| Input{passes: v})
+    }
+}
+
+fn parse_input(input: &str) -> Input {
+    input.parse().expect("Should be able to parse the input")
+}
+
+pub fn part1() -> u16 {
+    let input = parse_input(include_str!("inputs/day5.txt"));
+    *input.passes.iter().max().unwrap()
+}
+
+#[test]
+fn part1_works() {
+    assert_eq!(part1(), 930)   
+}
--- 2020/src/main.rs
+++ 2020/src/main.rs
@@ -2,6 +2,7 @@ mod day1;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
 
 fn main() {
     println!("Day 1 Part 1: {}", day1::part1());
@@ -12,4 +13,6 @@ fn main() {
     println!("Day 3 Part 2: {}", day3::part2());
     println!("Day 4 Part 1: {}", day4::part1());
     println!("Day 4 Part 2: {}", day4::part2());
+    println!("Day 5 Part 1: {}", day5::part1());
+    //println!("Day 5 Part 2: {}", day5::part2());
 }
