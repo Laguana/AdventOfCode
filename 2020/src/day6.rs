3154e96fdb27867af43197278c460ed3b8a3ac00
Laguana <laguana@redacted>
2022-01-17 22:51:47 -0800

    Day 6 Part 1 done

--- 2020/src/day6.rs
+++ 2020/src/day6.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::str::FromStr;
 
 #[test]
@@ -8,7 +9,7 @@ fn part1_sample_works() {
 }
 
 struct Input {
-    groups: Vec<Vec<String>>
+    groups: Vec<Vec<String>>,
 }
 
 #[derive(Debug)]
@@ -17,15 +18,13 @@ struct ParseError;
 impl FromStr for Input {
     type Err = ParseError;
     fn from_str(s: &str) -> std::result::Result<Self, <Self as std::str::FromStr>::Err> {
-        let x = s.replace("\r","")
+        let x = s
+            .replace("\r", "")
             .split("\n\n")
-            .map(|e| 
-                e.split("\n")
-                    .map(|s| s.to_string())
-                    .collect())
+            .map(|e| e.split("\n").map(|s| s.to_string()).collect())
             .collect();
-        
-        Ok(Input{groups: x})
+
+        Ok(Input { groups: x })
     }
 }
 
@@ -34,10 +33,23 @@ fn parse_input(s: &str) -> Input {
 }
 
 fn sum_common(input: &Input) -> usize {
-    0
+    input.groups.iter().map(|v| {
+        let s = v.iter().fold(HashSet::new(), |acc, e| {
+            let se: HashSet<char> = e.chars().collect();
+            acc.union(&se).cloned().collect()
+        });
+        
+        println!("{:?} -> {:?}", v, s);
+        return s.iter().count()
+    }).fold(0, |acc, b| acc + b)
 }
 
 pub fn part1() -> usize {
     let input = parse_input(include_str!("inputs/day6.txt"));
     sum_common(&input)
+}
+
+#[test]
+fn part1_works() {
+    assert_eq!(part1(), 6768)
 }
