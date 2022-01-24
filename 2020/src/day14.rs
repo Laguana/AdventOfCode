f0e81c4e35730151edd24f26ba07cf78735d61de
Laguana <laguana@redacted>
2022-01-23 21:09:53 -0800

    Day 14 Part 2

--- 2020/src/day14.rs
+++ 2020/src/day14.rs
@@ -91,3 +91,163 @@ pub fn part1() -> u64 {
 fn part1_works() {
     assert_eq!(part1(), 6317049172545)
 }
+
+// So this is what I thought would work.
+// And it did! But slow.
+/*
+
+#[derive(Debug, Eq, PartialEq, Clone)]
+enum MemTree {
+    Leaf(u64),
+    X(Box<MemTree>),
+    Branch(Box<MemTree>, Box<MemTree>)
+}
+
+fn initialize_memory() -> MemTree {
+    let mut ret = MemTree::Leaf(0);
+
+    for _ in 0..36 {
+        let ret_next = MemTree::X(Box::new(ret));
+        ret = ret_next;
+    }
+
+    return ret;
+}
+
+fn set_with_mask(memory: &MemTree, mask: &[MaskBit;36], address: u64, depth: usize, value: u64) -> MemTree {
+    match memory {
+        MemTree::Leaf(_) => {
+            MemTree::Leaf(value)
+        },
+        MemTree::X(t) => {
+            let t2 = set_with_mask(t, mask, address, depth+1, value);
+            match mask[depth] {
+                MaskBit::X => {
+                    MemTree::X(Box::new(t2))
+                },
+                MaskBit::On => {
+                    MemTree::Branch(t.clone(), Box::new(t2))
+                },
+                MaskBit::Off => {
+                    let bit_index = 35-depth;
+                    let bit = (address >> bit_index) & 1;
+                    if bit == 0 {
+                        MemTree::Branch(Box::new(t2),  t.clone())
+                    } else {
+                        MemTree::Branch(t.clone(),  Box::new(t2))
+                    }
+                }
+            }
+        },
+        MemTree::Branch(l,r) => {
+            match mask[depth] {
+                MaskBit::X => {
+                    let l2 = set_with_mask(l, mask, address, depth+1, value);
+                    let r2 = set_with_mask(r, mask, address, depth+1, value);
+                    MemTree::Branch(Box::new(l2), Box::new(r2))
+                },
+                MaskBit::On => {
+                    let r2 = set_with_mask(r, mask, address, depth+1, value);
+                    MemTree::Branch(l.clone(), Box::new(r2))
+                },
+                MaskBit::Off => {
+                    let bit_index = 35-depth;
+                    let bit = (address >> bit_index) & 1;
+                    if bit == 0 {
+                        let l2 = set_with_mask(l, mask, address, depth+1, value);
+                        MemTree::Branch(Box::new(l2),  r.clone())
+                    } else {
+                        let r2 = set_with_mask(r, mask, address, depth+1, value);
+                        MemTree::Branch(l.clone(),  Box::new(r2))
+                    }
+                }
+            }
+        }
+    }
+    // 
+}
+
+fn sum_tree_values(memory: &MemTree) -> u64 {
+    match memory {
+        MemTree::Leaf(x) => *x,
+        MemTree::Branch(l,r) => sum_tree_values(l) + sum_tree_values(r),
+        MemTree::X(t) => 2*sum_tree_values(t)
+    }
+}
+
+fn sum_part2_addresses(input: &Vec<Instruction>) -> u64 {
+    // mask[i] is 0 -> no change
+    //            1 -> addr[i] is on
+    //            X -> addr[i] is both
+    // represent memory as a 36-deep tree
+
+    let mut memory = initialize_memory();
+    let mut mask = [MaskBit::Off; 36];
+    
+    for i in input {
+        match i {
+            Instruction::Mask(bits) => {
+                mask = *bits;
+            },
+            Instruction::Assign(to, v) => {
+                memory = set_with_mask(&memory, &mask, *to, 0, *v);
+            }
+        }
+    }
+    sum_tree_values(&memory)
+}
+*/
+
+// This is the 'dumb' approach of just adding all the different values, but it worked much better
+// probably because the masks tended to have few Xs
+fn sum_part2_addresses_2(input: &Vec<Instruction>) -> u64 {
+    let mut memory = HashMap::new();
+    let mut mask = [MaskBit::Off; 36];
+
+    for i in input {
+        match i {
+            Instruction::Mask(bits) => {
+                mask = *bits;
+            },
+            Instruction::Assign(to, v) => {
+                for k in (0..36).fold(vec![0], |acc: Vec<u64>, e| {
+                    let address_bit = (to >> (35-e)) & 1;
+                    match mask[e] {
+                        MaskBit::Off => {
+                            // bit is address_bit
+                            acc.into_iter().map(|v| v<<1|address_bit).collect()
+                        },
+                        MaskBit::On => {
+                            acc.into_iter().map(|v| v<<1|1).collect()
+                        },
+                        MaskBit::X => {
+                            // bit is both
+                            acc.into_iter().map(|v| vec![v<<1|0, v<<1|1]).flatten().collect()
+                        }
+                    }
+                }) {
+                    memory.insert(k,v);
+                }
+            }
+        }
+    }
+
+    return memory.values().fold(0, |acc, &v| acc+v)
+}
+
+#[test]
+fn part2_sample_works() {
+    let input = parse_input(include_str!("inputs/day14.sample2"));
+    let result= sum_part2_addresses_2(&input);
+    assert_eq!(result, 208)
+}
+
+pub fn part2() -> u64 {
+    let input = parse_input(include_str!("inputs/day14.txt"));
+    sum_part2_addresses_2(&input)
+}
+
+#[test]
+fn part2_works() {
+    assert_eq!(part2(), 3434009980379)
+}
--- 2020/src/main.rs
+++ 2020/src/main.rs
@@ -40,6 +40,6 @@ fn main() {
     println!("Day 12 Part 2: {}", day12::part2());
     println!("Day 13 Part 1: {}", day13::part1());
     println!("Day 13 Part 2: {}", day13::part2());
-    //println!("Day 14 Part 1: {}", day14::part1());
-    //println!("Day 14 Part 2: {}", day14::part2());
+    println!("Day 14 Part 1: {}", day14::part1());
+    println!("Day 14 Part 2: {}", day14::part2());
 }
