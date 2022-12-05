1bdde024b41d79198efded8fc6b19d79cd93bcf8
Laguana <laguana@redacted>
2022-12-04 20:40:03 -0800

    Scaffolding day5

--- 2022/src/main.rs
+++ 2022/src/main.rs
@@ -2,6 +2,7 @@ mod day1;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
 
 fn main() {
     println!("Day1 Part 1 result: {}", day1::part1());
@@ -15,5 +16,8 @@ fn main() {
 
     println!("Day4 Part 1 result: {}", day4::part1());
     println!("Day4 Part 2 result: {}", day4::part2());
+
+    println!("Day5 Part 1 result: {}", day5::part1());
+    println!("Day5 Part 2 result: {}", day5::part2());
 
 }
