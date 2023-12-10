a1f3a5b80fa849fe9753495552e58ebe197c77e2
Laguana <laguana@redacted>
2023-12-10 14:48:24 -0800

    Day 10 part2 done

--- 2023/day10/day10.pl
+++ 2023/day10/day10.pl
@@ -83,42 +83,72 @@ connected_distances(G, [q(Pos, Prev, Dist)|T], DIn, DOut, MaxDist) :-
 part1(In, Out) :-
     connected_distances(In, _, Out).
 
-toggle(out, in).
-toggle(in, out).
+% State is out, n, s, in
 
-enclosed_step(ew, State, State) :-
-    !, print('-').
-enclosed_step(ns, out, in) :- !, print('|').
-enclosed_step(ns, in, out) :- !, print('|').
 
-enclosed_step(ne, out, ne) :- !, print('#').
-enclosed_step(ne, in, ine) :- !, print('@').
+enclosed_step(ew, State, State). % :- print('-').
+enclosed_step(ns, out, in). % :- print('i').
+enclosed_step(ns, in, out). % :- print('i').
+% Not legal to be n or s
 
-enclosed_step(se, out, se) :- !, print('#').
-enclosed_step(se, in, ise) :- !, print('@').
+enclosed_step(ne, out, n). % :- print('\\').
+enclosed_step(ne, in, s). % :- print('\\').
+enclosed_step(se, out, s). % :- print('/').
+enclosed_step(se, in, n). % :- print('/').
+% Not legal to be n or s
 
-enclosed_step(nw, se, out) :- !, print('#').
-enclosed_step(nw, ne, )
+enclosed_step(nw, n, out). % :- print('/').
+enclosed_step(nw, s, in). % :- print('/').
+enclosed_step(sw, n, in). % :- print('\\').
+enclosed_step(sw, s, out). % :- print('\\').
+% Not legal to be in or out
 
+is_one_step(MainLoop, Pos) :-
+    pos_key(Pos, K),
+    MainLoop.get(K) = 1.
+
+pos_sub(pos(X,Y), pos(X1,Y1), delta(DX, DY)) :-
+    DX is X1-X,
+    DY is Y1-Y.
+
+delta_to_tile([delta(-1, 0), delta(0, -1)], nw).
+delta_to_tile([delta(-1, 0), delta(0, 1)], sw).
+delta_to_tile([delta(-1, 0), delta(1, 0)], ew).
+delta_to_tile([delta(0, -1), delta(0, 1)], ns).
+delta_to_tile([delta(0, -1), delta(1, 0)], se).
+delta_to_tile([delta(1, 0), delta(0, -1)], ne).
+
+
+
+effective_tile(start, MainLoop, X, Y, Tile) :- !,
+    bagof(Adj, adjacent(pos(X,Y),Adj), Adjacent),
+    include(is_one_step(MainLoop), Adjacent, OneStep),
+    maplist(pos_sub(pos(X,Y)), OneStep, Deltas),
+    sort(Deltas, SDeltas),
+    delta_to_tile(SDeltas, Tile).
+effective_tile(Tile, _, _, _, Tile).
 
 count_enclosed_line(G, MainLoop, Y, Count) :-
+    %print(Y), print(':'),
     count_enclosed_line(G, MainLoop, Y, G.xmax, out, 0, 0, Count),
-    !, nl.
+    !.
 count_enclosed_line(_, _, _, XMax, _, X, Count, Count) :-
     X >= XMax, !.
 count_enclosed_line(G, MainLoop, Y, XMax, State, X, Acc, Count) :-
     pos_key(pos(X,Y), K),
     _ is MainLoop.get(K), !,
     Xn is X+1,
-    enclosed_step(G.get(K), State, NState),
+    effective_tile(G.get(K),MainLoop, X, Y, Tile),
+    enclosed_step(Tile, State, NState),
     count_enclosed_line(G, MainLoop, Y, XMax, NState, Xn, Acc, Count).
 count_enclosed_line(G, MainLoop, Y, XMax, State, X, Acc, Count) :-
     Xn is X+1,
-    (State = out -> NAcc = Acc, print(0); NAcc is Acc+1, print(1)),
+    (State = out -> NAcc = Acc; State = in, NAcc is Acc+1),
     count_enclosed_line(G, MainLoop, Y, XMax, State, Xn, NAcc, Count).
 
 count_enclosed(G, MainLoop, Out) :- 
     setof(Y, between(0,G.ymax, Y), Ys),!,
+    %print(start), nl,
     maplist(count_enclosed_line(G,MainLoop), Ys, Counts),
     !,
     sumlist(Counts, Out).
@@ -132,6 +162,6 @@ part2(In, Out) :-
 
 :- phrase_from_file(input(I), "day10.in"), part1(I, 6773).%Out), print(Out).
 
-:- phrase_from_file(input(I), "day10.example2.in"),  part2(I, X), print(X).
+:- phrase_from_file(input(I), "day10.example2.in"),  part2(I, 10).
 
-%:- phrase_from_file(input(I), "day10.in"), part2(I, 908).%Out), print(Out).
+:- phrase_from_file(input(I), "day10.in"), part2(I, 493).%Out), print(Out).
