8058bce1f670e943c63925f193e8037cbdbf8b3f
Laguana <laguana@redacted>
2024-12-22 11:44:15 -0800

    Speed things up ~800x by being a little less dumb

--- 2024/day22/day22_lib.cc
+++ 2024/day22/day22_lib.cc
@@ -59,14 +59,12 @@ uint64_t make_key(int d0, int d1, int d2, int d3) {
 }
 
 // what if i just be dumb?
-std::unordered_map<uint64_t, int> find_sequences(int64_t seed) {
-    std::unordered_set<int64_t> seen;
-    std::unordered_map<uint64_t, int> delta_results;
+void find_sequences(int64_t seed, std::unordered_map<uint64_t, int64_t> &delta_results) {
+    std::unordered_set<uint64_t> seen;
 
     int d0=-10, d1=-10, d2=-10, d3=-10;
     int steps = 2000;
-    while (!seen.contains(seed) && --steps != 0) {
-        seen.insert(seed);
+    while (--steps != 0) {
         auto next = evolve(seed, 1);
 
         auto digit = next%10;
@@ -77,48 +75,28 @@ std::unordered_map<uint64_t, int> find_sequences(int64_t seed) {
 
         if (d3 > -10) {
             auto key = make_key(d0, d1, d2, d3);
-            if(!delta_results.contains(key)) {
-                if (d1 < -5 && d2 < -5) {
-                    std::cout << seed << " -> " << next << std::endl;
-                }
-                delta_results[key] = digit;
+            if(!seen.contains(key)) {
+                delta_results[key] += digit;
             }
+            seen.insert(key);
         }
         
         seed = next;
     }
-
-    return delta_results;
 }
 
 int64_t Input::buy_bananas() const {
     int64_t result = 0;
 
-    std::vector<std::unordered_map<uint64_t, int>> delta_results;
+    std::unordered_map<uint64_t, int64_t> delta_results;
     for(auto seed: secrets) {
         //std::cout << "Computing deltas for " << seed << std::endl;
-        delta_results.push_back(find_sequences(seed));
+        find_sequences(seed, delta_results);
     }
 
-    for(int d1 = -9; d1 < 10; ++d1) {
-        for (int d2 = -9; d2 < 10; ++d2) {
-            for (int d3 = -9; d3 < 10; ++d3) {
-                for (int d4 = -9; d4 < 100; ++d4) {
-                    int64_t sum = 0;
-                    auto key = make_key(d1, d2, d3, d4);
-                    for(const auto &m: delta_results ) {
-                        auto it = m.find(key);
-                        if (it != m.end()) {
-                            sum += it->second;
-                        }
-                    }
-                    if (sum > result) {
-                        std::cout << d1 << "," << d2 << "," << d3 << "," << d4 << "~" << std::hex << key << std::dec << " new best " << sum << std::endl;
-                    }
-                    result = std::max(result, sum);
-                }
-            }
-        }
+    for (auto delta: delta_results) {
+        //std::cout << std::hex << delta.first << std::dec << " = " << delta.second << std::endl;
+        result = std::max(result, delta.second);
     }
 
     return result;
