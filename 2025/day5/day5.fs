a6f8f0e595deec40a1ea4afb120c6fe3a0235408
Laguana <laguana@redacted>
2025-12-04 23:48:28 -0800

    Day 5 part 2 done.

--- 2025/day5/day5.fs
+++ 2025/day5/day5.fs
@@ -65,7 +65,7 @@ VARIABLE day-5-ranges
         dup 2@ 
         ( rangeptr low high)
         n > swap n < or if
-            2 cells + 
+            2 cells +
         else
             drop unloop true exit
         endif
@@ -93,8 +93,81 @@ VARIABLE day-5-ranges
     drop
 ;
 
+: sort-ranges ( -- ) \ in-place insertion sort is fine right?
+    \ first element is already inserted, insert the rest
+    day-5-num-ranges @ 1 DO
+        \ we have inserted prior to index i already, now we are inserting i
+        i
+        day-5-ranges @ i 2 * cells + >r
+        r@ 2@
+        \ S" Outer loop" type .S cr
+        ( i h l )
+        rot 0 DO
+            \ if it is smaller, swap the one we have with the slot we're looking at
+            \ i is now the inner loop, j the outer
+            day-5-ranges @ i 2 * cells + >r
+            r@ @
+            ( h l x)
+            \ S" Inner loop" type .S cr key drop
+            over > if
+                \ we do want to swap
+                r@ 2@
+                ( h l xh xl)
+                2swap r@ 2!
+            else
+                r@ @ over = if
+                    ( h l ) \ we might want to swap if the high is lower
+                    r@ 1+ @
+                    ( h l y )
+                    2 pick > if
+                        ( h l ) \ we do want to swap
+                        r@ 2@
+                        ( h l xh xl )
+                        2swap r@ 2!
+                    endif
+                endif
+            endif
+            r> drop
+        LOOP
+        ( h l )
+        r> 2!
+    LOOP
+;
+
 : day-5-part-2 ( fd -- answer )
-    parse-ranges
+    parse-ranges drop
+    sort-ranges
+
+    0 0
+    day-5-num-ranges @ 0 DO
+        ( acc previous-high )
+        \ S" Start loop" type .S cr
+        day-5-ranges @ i 2 * cells + 2@
+        ( acc previous-high high low )
+        rot
+        ( acc high low previous-high )
+        2dup <= if
+            \ this range intersects
+            \ S" Range intersects" type .S cr
+            ( acc high low previous-high )
+            nip
+            1+
+            ( acc high new-low)
+        else
+            drop
+        endif
+        ( acc high low )
+        2dup >= if
+            \ after we adjusted the range, it is still nonempty
+            over swap - 1+
+            ( acc high high-low )
+            \ S" Found a range " type .S cr
+            rot + swap
+        else
+            drop
+        endif
+    LOOP
+    drop
 ;
 
 : test-day-5-part-1
@@ -117,12 +190,12 @@ VARIABLE day-5-ranges
 : test-day-5-part-2
   s" day5.example" r/o open-file throw ( -- fd )
   >r
-  r@ day-5-part-2 . \ assert( 14 = )
+  r@ day-5-part-2  assert( 14 = )
   r>
   close-file throw
 ;
 
-( Expect to get ? )
+( Expect to get 369761800782619 )
 : do-day-5-part-2
   s" day5.in" r/o open-file throw ( -- fd )
   >r
